3d6be288fb1e73201277a79631e34bf7bc2bd081
Connor McLaughlin <stenzek@gmail.com>
2020-07-23 16:41:33 +1000

    Add update installer program (Windows only for now)

---
+++ src/updater/win32_main.cpp
@@ -0,0 +1,112 @@
+#include "common/file_system.h"
+#include "common/log.h"
+#include "common/string_util.h"
+#include "common/win32_progress_callback.h"
+#include "common/windows_headers.h"
+#include "updater.h"
+#include <shellapi.h>
+
+static std::string WideCharStringToStdString(const PWSTR str)
+{
+  const size_t wide_len = std::wcslen(str);
+  int u8len = WideCharToMultiByte(CP_UTF8, 0, str, static_cast<int>(wide_len), nullptr, 0, nullptr, nullptr);
+  std::string result;
+  if (u8len > 0)
+  {
+    result.resize(u8len);
+    u8len = WideCharToMultiByte(CP_UTF8, 0, str, static_cast<int>(wide_len), result.data(),
+                                static_cast<int>(result.size()), 0, nullptr);
+    if (u8len > 0)
+      result.resize(u8len);
+    else
+      result.clear();
+  }
+
+  return result;
+}
+
+static void WaitForProcessToExit(int process_id)
+{
+  HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, process_id);
+  if (!hProcess)
+    return;
+
+  WaitForSingleObject(hProcess, INFINITE);
+  CloseHandle(hProcess);
+}
+
+int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+{
+  Log::SetConsoleOutputParams(true);
+
+  Win32ProgressCallback progress;
+
+  int argc = 0;
+  LPWSTR* argv = CommandLineToArgvW(lpCmdLine, &argc);
+  if (!argv || argc <= 0)
+  {
+    progress.ModalError("Failed to parse command line.");
+    return 1;
+  }
+  if (argc != 4)
+  {
+    progress.ModalError("Expected 4 arguments: parent process id, output directory, update zip, program to launch.");
+    LocalFree(argv);
+    return 1;
+  }
+
+  const int parent_process_id = StringUtil::FromChars<int>(WideCharStringToStdString(argv[0])).value_or(0);
+  const std::string destination_directory = WideCharStringToStdString(argv[1]);
+  const std::string zip_path = WideCharStringToStdString(argv[2]);
+  const std::string program_to_launch = WideCharStringToStdString(argv[3]);
+  LocalFree(argv);
+
+  if (parent_process_id <= 0 || destination_directory.empty() || zip_path.empty() || program_to_launch.empty())
+  {
+    progress.ModalError("One or more parameters is empty.");
+    return 1;
+  }
+
+  progress.SetFormattedStatusText("Waiting for parent process %d to exit...", parent_process_id);
+  WaitForProcessToExit(parent_process_id);
+
+  Updater updater(&progress);
+  if (!updater.Initialize(destination_directory))
+  {
+    progress.ModalError("Failed to initialize updater.");
+    return 1;
+  }
+
+  if (!updater.OpenUpdateZip(zip_path.c_str()))
+  {
+    progress.DisplayFormattedModalError("Could not open update zip '%s'. Update not installed.", zip_path.c_str());
+    return 1;
+  }
+
+  if (!updater.PrepareStagingDirectory())
+  {
+    progress.ModalError("Failed to prepare staging directory. Update not installed.");
+    return 1;
+  }
+
+  if (!updater.StageUpdate())
+  {
+    progress.ModalError("Failed to stage update. Update not installed.");
+    return 1;
+  }
+
+  if (!updater.CommitUpdate())
+  {
+    progress.ModalError(
+      "Failed to commit update. Your installation may be corrupted, please re-download a fresh version from GitHub.");
+    return 1;
+  }
+
+  updater.CleanupStagingDirectory();
+
+  progress.ModalInformation("Update complete.");
+
+  progress.DisplayFormattedInformation("Launching '%s'...", program_to_launch.c_str());
+  ShellExecuteA(nullptr, "open", program_to_launch.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
+  return 0;
+}
